5c58f197a1aa8665019bab8c95c7c8804a1567c2
iguoliveira <igor.or042@gmail.com>
2022-07-12 14:27:56 -0300

    Start of the login page

---
+++ src/components/loginPage/button/button.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <button>{{btnName}}</button>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'buttonData',
+    props: {
+        btnName: String
+    },
+}
+</script>
+
+<style scoped>
+button{
+    border: 0;
+    background-color: #5217d1;
+    color: white;
+    cursor: pointer;
+    padding: 5px 10px;
+}
+button:hover{
+    background-color: #5924c9;
+}
+</style>
---
+++ src/components/loginPage/input/input.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <label>{{label}}</label>
+    <input :placeholder="inputName" />
+</div>
+</template>
+
+<script>
+export default{
+    name: 'inputData',
+    props: {
+        inputName: String,
+        label: String,
+    },
+}
+</script>
+
+<style scoped>
+div{
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    padding: 10px;
+}
+input{
+    border: 0;
+    border-bottom: 1px solid black;
+    background-color: transparent;
+}
+input:selector{
+    
+}
+</style>
