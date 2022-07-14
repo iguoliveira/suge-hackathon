773647b97457cd8020e1b7aa6cf78e76ac2e1fe1
iguoliveira <igor.or042@gmail.com>
2022-07-14 11:09:03 -0300

    Initial of the homepage. Finded the background color of bosch pattern

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <template>
-  <navbar />
+  <homepage />
 </template>
 
 <style>
--- src/components/homepage/homepage.vue
+++ src/components/homepage/homepage.vue
@@ -1,15 +1,23 @@
 <template>
-    <div>
-        salve
-    </div>
+  <div>
+      <boschLine />
+      <navbar />
+  </div>
 </template>
 
 <script>
-export default{
-    name: 'homepage',
-}
+import boschLine from "../global-items/boschFade.vue";
+import navbar from "../navbar/navbar.vue";
+
+export default {
+  name: "homepage",
+  components: {
+    boschLine,
+    navbar,
+  },
+};
 </script>
 
 <style scoped>
-
+   /* background-color: #eff1f2  BOSCH CONTENT BACKGROUND COLOR LINK: https://brandguide.bosch.com/document/337#/farben */
 </style>
